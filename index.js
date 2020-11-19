6e6c0631e1de017ef32cba22f68b4bf40c3b0643
Andrew McManus <andrewmcmanusmusic@gmail.com>
2020-11-19 17:00:41 -0500

    end of day 11_19

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
+const db = require('./models');
 
 // app
 const app = express();
@@ -32,12 +33,77 @@ app.get('/', (req, res) => {
           date: movieResultObject.publication_date,
           url: movieResultObject.link.url
         }
-        console.log(finalObject);
+        // add each movie to the database inside of (movies)
+        db.movie.findOrCreate({
+          where: { title: finalObject.title },
+          defaults: {
+            byline: finalObject.byline,
+            headline: finalObject.headline,
+            date: finalObject.date,
+            url: finalObject.url
+          }
+        }).then(([movie, created]) => {
+          // res.send(movie.get().title);
+          console.log(created);
+        })
       }
       // display_title, byline, headline, publication_date, link.url
     }
   }).catch(err => {
     console.log(err);
+  })
+})
+
+app.get('/getrocky', (req, res) => {
+  axios.get(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=rocky&api-key=${NYT_API_KEY}`).then(response => {
+    if (response.status === 200) {
+      // console.log(response.data.results);
+      let len = response.data.results.length;
+
+      for (let i=0; i < len; i++) {
+        let movieResultObject = response.data.results[i];
+        // create a model for the data that will be in the database!
+        const finalObject = {
+          title: movieResultObject.display_title,
+          byline: movieResultObject.byline,
+          headline: movieResultObject.headline,
+          date: movieResultObject.publication_date,
+          url: movieResultObject.link.url
+        }
+        // add each movie to the database inside of (movies)
+        db.movie.findOrCreate({
+          where: { title: finalObject.title },
+          defaults: {
+            byline: finalObject.byline,
+            headline: finalObject.headline,
+            date: finalObject.date,
+            url: finalObject.url
+          }
+        }).then(([movie, created]) => {
+          // res.send(movie.get().title);
+          console.log(created);
+        })
+      }
+      // display_title, byline, headline, publication_date, link.url
+    }
+  }).catch(err => {
+    console.log(err);
+  })
+
+})
+
+app.get('/rocky', (req, res) => {
+  db.movie.findOne({
+    where: { title: 'Rocky II'}
+  }).then(rockyMovie => {
+    res.send(rockyMovie);
+  })
+})
+
+// godfather route
+app.get('/godfather', (req, res) => {
+  db.movie.findAll().then(moviesArray => {
+    console.log(moviesArray);
   })
 })
 
